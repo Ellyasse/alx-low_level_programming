e01c2985c76366f29ba364b7576c6c3e68be4847
Faissal <ellyassef@gmail.com>
2023-06-30 19:40:27 -0700

    ok

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,27 +1,51 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
+ * infinite_add - Adds two numbers
+ * @n1: First number as a string
+ * @n2: Second number as a string
+ * @r: Buffer to store the result
+ * @size_r: Size of the buffer
+ *
+ * Return: Pointer to the result (r) or 0 if the result cannot be stored in r
  */
-
-void rev_string(char *n)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0;
-	int j = 0;
-	char temp;
+    int len1 = strlen(n1);
+    int len2 = strlen(n2);
+    int carry = 0;
+    int i, j;
 
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
+    if (len1 + 1 > size_r || len2 + 1 > size_r)
+        return 0;
 
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
+    i = len1 - 1;
+    j = len2 - 1;
+    r[size_r - 1] = '\0';
+
+    while (i >= 0 || j >= 0 || carry)
+    {
+        int digit1 = (i >= 0) ? (n1[i] - '0') : 0;
+        int digit2 = (j >= 0) ? (n2[j] - '0') : 0;
+        int sum = digit1 + digit2 + carry;
+
+        if (sum >= 10)
+        {
+            carry = 1;
+            sum -= 10;
+        }
+        else
+        {
+            carry = 0;
+        }
+
+        r[size_r - 2] = sum + '0';
+        i--;
+        j--;
+        size_r--;
+    }
+
+    return &r[size_r - 1];
 }
