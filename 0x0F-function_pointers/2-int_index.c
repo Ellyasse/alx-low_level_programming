7f0cb1d8e4156c13afbb69130cfb9e2fcca27f16
Faissal <ellyassef@gmail.com>
2023-07-19 16:21:01 -0700

    all at once

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+/**
+ * int_index - this functions find the index of a int
+(* a blank line
+ *@array: the array to find the index
+ *@size: the size of the array
+ *@cmp: this a function pointer.
+* Description: this function find the index of a int)?
+(* section header: the header of this function is function_pointers.h)*
+* Return: this is a void function no return
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, r;
+
+	if (size > 0 && array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			r = cmp(array[i]);
+			if (r)
+				break;
+		}
+		if (i < size)
+			return (i);
+	}
+	return (-1);
+}
+
