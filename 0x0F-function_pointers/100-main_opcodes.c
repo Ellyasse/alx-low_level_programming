74c313166b2842a5111a342682d7e4fee3c27cf6
Faissal <ellyassef@gmail.com>
2023-07-19 16:28:05 -0700

    last ex

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,31 +10,29 @@
  */
 int main(int argc, char *argv[])
 {
-	char *opc = (char *) main;
-	int i, nbytes;
+	int i, n;
+
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
-
-	nbytes = atoi(argv[1]);
-
-	if (nbytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	for (i = 0; i < nbytes; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%02x", opc[i] & 0xFF);
-		if (i != nbytes - 1)
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
 			printf(" ");
+		else
+			printf("\n");
 	}
-
-	printf("\n");
 	return (0);
 }
 
